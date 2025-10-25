3578a9ccc0e3a470d14cb19aa3117c6698aa97c4
nanoview <arif.js@gmail.com>
2025-10-25 15:33:29 +0300

    Button styles added to BookForm component

--- frontend/src/components/BookForm.jsx
+++ frontend/src/components/BookForm.jsx
@@ -17,7 +17,7 @@ export default function BookForm({ onSubmit }) {
              onChange={e => setBook({ ...book, author: e.target.value })} />
       <input placeholder="Year" value={book.year}
              onChange={e => setBook({ ...book, year: e.target.value })} />
-      <button type="submit">Add</button>
+      <button style={{ marginRight: 8, background: '#28a745', color: '#fff', border: 'none', padding: '6px 10px', borderRadius: 4 }}type="submit">Add</button>
     </form>
   );
 }
