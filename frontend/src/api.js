d2983b25097db4548f0d749a98d90ff7d6c80fc5
Ariful Islam <55536153+nanoview@users.noreply.github.com>
2026-01-23 14:44:48 +0200

    api url configuration

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const backendURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 export const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: backendURL
 });
 
 // Add auth token to requests
